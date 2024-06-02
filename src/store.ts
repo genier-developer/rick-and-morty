b8b4df9dcce5511f5951e2b66fb8a78afd571b10
Olga Genier <genier.developer@gmail.com>
2024-06-02 17:19:57 +0300

    add store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {rickAndMortyApi} from "./api/rickAndMortyApi.ts";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(rickAndMortyApi.middleware),
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
