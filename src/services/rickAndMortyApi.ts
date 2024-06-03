840c7a1bcecfa5132f810b99b00c4d7a9bf0a997
Olga Genier <genier.developer@gmail.com>
2024-06-03 18:09:37 +0300

    fix bugs with pagination

--- src/services/rickAndMortyApi.ts
+++ src/services/rickAndMortyApi.ts
@@ -1,11 +1,28 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
+
+interface CharactersResponse {
+    info: {
+        count: number;
+        pages: number;
+        next: string | null;
+        prev: string | null;
+    };
+    results: Character[];
+}
+
+interface Character {
+    id: number;
+    name: string;
+    status: string;
+    image: string;
+}
 export const rickAndMortyApi = createApi({
     reducerPath: 'rickAndMortyApi',
     baseQuery: fetchBaseQuery({baseUrl: 'https://rickandmortyapi.com/api'}),
     endpoints: (builder) => ({
-        getCharacters: builder.query({
-            query: (page=1, name='', status='') => `character/?page=${page}&name=${name}&status=${status}`,
+        getCharacters: builder.query<CharactersResponse, {page?: number, name?: string, status?: string}>({
+            query: ({page= 1, name='', status=''}) => `character/?page=${page}&name=${name}&status=${status}`,
         }),
     }),
 })
